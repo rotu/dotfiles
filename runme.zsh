7773bcadc4571559edf08fa62d06533af3e1ee00
Dan <DanOfTheRoses@gmail.com>
2022-04-20 05:26:16 -0500

    fixed install script

--- runme.zsh
+++ runme.zsh
@@ -6,4 +6,4 @@ ln -s ${0:a:h}/gitconfig $HOME/.gitconfig
 ln -s ${0:a:h}/gitignore $HOME/.gitignore
 ln -s ${0:a:h}/hgignore $HOME/.hgignore
 ln -s ${0:a:h}/hgrc $HOME/.hgrc
-ln -s ${0:a:h}/config_kitty_ssh $HOME/.config/kitty/ssh.conf
+ln -s ${0:a:h}/config_kitty_ssh.conf $HOME/.config/kitty/ssh.conf
